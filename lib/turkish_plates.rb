50db0592049c95d019d2df3b7ac32c714de1eaee
Osman OKUYAN <f131220110@ktun.edu.tr>
2021-10-05 15:44:18 +0300

    initial commit

---
+++ lib/turkish_plates.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "turkish_plates/version"
+
+module TurkishPlates
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/turkish_plates/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module TurkishPlates
+  VERSION = "0.1.0"
+end
