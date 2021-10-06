ef7b466256928be977f7f0368ec00dae362fabb1
Osman OKUYAN <f131220110@ktun.edu.tr>
2021-10-06 09:02:14 +0300

    added data method

--- README.md
+++ README.md
@@ -27,4 +27,4 @@ To install this gem onto your local machine, run `bundle exec rake install`. To 
 
 ## Contributing
 
-Bug reports and pull requests are welcome on GitHub at https://github.com/[USERNAME]/turkish_plates.
+Bug reports and pull requests are welcome on GitHub at https://github.com/osmanok/turkish_plates.
--- lib/turkish_plates.rb
+++ lib/turkish_plates.rb
@@ -1,8 +1,17 @@
 # frozen_string_literal: true
 
+require "json"
+
 require_relative "turkish_plates/version"
 
 module TurkishPlates
-  class Error < StandardError; end
-  # Your code goes here...
+  def self.load_data
+    @data = JSON.parse(File.read("#{__dir__}/turkish_plates/data/plates.json"))
+  end
+
+  def self.data
+    @data ||= load_data
+  end
 end
+
+puts "Loaded #{TurkishPlates.data.size} plates"
